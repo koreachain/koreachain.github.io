2e87a1c497a95bc576ba0de73bf60564b11fa367
Silver Aloe <91305009+silveraloe@users.noreply.github.com>
2025-07-28 11:20:11 -0300

    Update link in homepage header for NFT airdrop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,9 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://www.tensor.trade/trade/plushylilpenguins"
+          >
+            🎁 Buy NFT for Airdrop
           </Link>
         </div>
       </div>
