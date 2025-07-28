56ccb860c0911ad32e517dd41b18cf0ff84c9b48
Silver Aloe <91305009+silveraloe@users.noreply.github.com>
2025-07-28 14:32:27 -0300

    Add color mode configuration to theme settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,6 +51,11 @@ const config: Config = {
   ],
 
   themeConfig: {
+    colorMode: {
+      defaultMode: 'light',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
